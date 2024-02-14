128f814face5e0a994785514eefbdbef008d2d2d
Caden Tedeschi <tedeschicaden@gmail.com>
2024-02-14 10:20:12 -0700

    Working on mechanics

--- Assets/Script/PlayerData.cs
+++ Assets/Script/PlayerData.cs
@@ -11,10 +11,15 @@ public struct PlayerData : IEquatable<PlayerData>, INetworkSerializable
     public int colorId;
     public FixedString64Bytes playerName;
     public FixedString64Bytes playerId;
+    public int playerHealth;
 
     public bool Equals(PlayerData other)
     {
-        return clientId == other.clientId && colorId == other.colorId && playerName == other.playerName && playerId == other.playerId;
+        return clientId == other.clientId
+            && colorId == other.colorId
+            && playerName == other.playerName
+            && playerId == other.playerId
+            && playerHealth == other.playerHealth;
     }
 
     public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
@@ -23,6 +28,7 @@ public struct PlayerData : IEquatable<PlayerData>, INetworkSerializable
         serializer.SerializeValue(ref colorId);
         serializer.SerializeValue(ref playerName);
         serializer.SerializeValue(ref playerId);
+        serializer.SerializeValue(ref playerHealth);
     }
     
 }
